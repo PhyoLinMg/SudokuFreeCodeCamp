1eea995d7a8a6b58dc0c559ed86211eb923f3cdc
Lin Maung <gangsterplm@gmail.com>
2021-09-08 23:11:03 +0630

    during ui layer 01:35:39 done

---
+++ app/src/main/java/dev/linmg/sudokufreecodecamp/ui/GraphSudokuTheme.kt
@@ -0,0 +1,2 @@
+package dev.linmg.sudokufreecodecamp.ui
+
--- app/src/main/java/dev/linmg/sudokufreecodecamp/ui/Shape.kt
+++ app/src/main/java/dev/linmg/sudokufreecodecamp/ui/Shape.kt
@@ -1,11 +1,12 @@
-package dev.linmg.sudokufreecodecamp.ui.theme
+package dev.linmg.sudokufreecodecamp.ui
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
 import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes(
+val shapes=Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    large = RoundedCornerShape(0.dp),
+
 )
